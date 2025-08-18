2b05184461362c305a3ef53a2d51e7c38a21fa38
JulaMRA <jul.milcz@gmail.com>
2025-08-18 19:43:13 +0200

    Add files via upload

--- TEST/js/scroll-reveal.js
+++ TEST/js/scroll-reveal.js
@@ -1,20 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const elements = document.querySelectorAll(".cocktails .container div");
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-        entry.target.classList.remove("hidden");
-      } else {
-        entry.target.classList.remove("show");
-        entry.target.classList.add("hidden");
-      }
-    });
-  }, { threshold: 0.1 });
-
-  elements.forEach(el => {
-    el.classList.add("hidden"); // start ukryty
-    observer.observe(el);
-  });
+document.querySelector('.burger').addEventListener('click', function() {
+  document.querySelector('.nav-list').classList.toggle('active');
 });
